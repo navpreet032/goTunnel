30c33a7835fff7b156747f7988f279a95627e420
codesmith <preetpannu032@gmail.com>
2026-02-02 20:00:55 +0530

    fix for reverse proxy config

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -101,7 +101,7 @@ func (c *Client) Register() error {
 	}
 
 	// Store our assigned details
-	c.clientID = regResp.ClientID
+	c.clientID = "client-" + regResp.ClientID
 	c.tunnelURL = regResp.TunnelURL
 
 	log.Printf("[INFO] ==========================================")
